eb4659e0fcf6731022b6a28b9268264808db71ca
Nasir Aftab <nasiraftab123@gmail.com>
2024-09-21 19:41:01 +0530

    Second commit

--- src/Components/Loading-Comp/loading-comp.jsx
+++ src/Components/Loading-Comp/loading-comp.jsx
@@ -5,12 +5,16 @@ import './loading-comp.scss'
 
 //imgs
 import LoadingImg from '../../img/cloudy.png';
+import LoadingGIF from '../../img/loaging-gif.gif';
+
 
 export default function LoadingComp() {
     return (
         <div className="loading-comp-main">
 
             <img src={LoadingImg} alt="" className="loading-img" />
+            <img src={LoadingGIF} alt="" className="loading-gifS" />
+
 
         </div>
     )
